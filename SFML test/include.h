b5b32d9ca12d7ad385931bfd305cdd5664ebd249
Stryder-Crouse <100306557+Stryder-Crouse@users.noreply.github.com>
2022-02-23 12:52:19 -0500

    Add files via upload

---
+++ SFML test/include.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <chrono> 
+#include <stdlib.h>
+#include <time.h>
+#include <random>
+#include <windows.h>
+#include <sstream>
+#include <vector>
+#include<math.h>
+#include<fstream>
+#include <sstream>
+#include<fstream>
